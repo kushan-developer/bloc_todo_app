d88f16cfbfd00b93615278c6e18154cc54b9b390
Kushan Chatterjee <45503802+kushan-developer@users.noreply.github.com>
2022-01-31 11:09:39 +0530

    Updated constants.dart

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,3 +1,3 @@
 const String addTodoRoute = "/add";
 const String editTodoRoute = "/edit";
-const String apiBase = "http://192.168.29.202:3000";
+const String apiBase = "<API_URL_PROVIDED_IN_JSON_SERVER>"; // example: http://192.168.29.202:3000
